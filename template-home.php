e60caad233ab9479f45f1e065c2637165df6dd8d
Aimee <aimeebethmj@gmail.com>
2015-10-31 11:19:56 +0000

    Copied in code from index file to pull posts- needs tweaking

--- template-home.php
+++ template-home.php
@@ -50,7 +50,45 @@ get_header(); ?>
     <?php endif; ?>
 
 
+<!-- PULL IN POSTS -->
 
+<section id="page-content-container" class="blog-post-archive">
+        
+
+    
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
+
+        <?php if ( have_posts() ) : ?>
+
+            <?php /* Start the Loop */ ?>
+            <?php while ( have_posts() ) : the_post(); ?>
+
+                <?php
+                    /* Include the Post-Format-specific template for the content.
+                     * If you want to override this in a child theme, then include a file
+                     * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+                     */
+                    get_template_part( 'content', get_post_format() );
+                ?>
+
+            <?php endwhile; ?>
+
+            <?php the_posts_navigation(); ?>
+
+        <?php else : ?>
+
+            <?php get_template_part( 'content', 'none' ); ?>
+
+        <?php endif; ?>
+
+        </main><!-- #main -->
+    </div><!-- #primary -->
+
+
+</section>
+
+<!-- 
 	<section id="home-works">
 		
         <div class="grid grid-pad">
@@ -61,21 +99,21 @@ get_header(); ?>
         	</div>
     	</div>
         
-	<div class="grid grid-pad">
+	<div class="grid grid-pad"> -->
     
      <?php $home_projects_number = esc_attr( get_theme_mod( 'luna_home_projects_number', '8' )) ?>
     
      <?php 
 	 
 		// the query
-		$the_query = new WP_Query( 'post_type=project', 'posts_per_page='. $home_projects_number ); ?>
-		
-		<?php if ( $the_query->have_posts() ) : ?> 
+		// $the_query = new WP_Query( 'post_type=project', 'posts_per_page='. $home_projects_number ); ?>
+<!-- 		
+		<?php if ( $the_query->have_posts() ) : ?>  -->
 		
 				<!-- pagination here -->
 		
 				<!-- the loop -->
-				<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+<!-- 				<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
             
             		<?php if (has_post_thumbnail( $post->ID ) ): ?>
 					<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'luna-home-project' ); 
@@ -95,22 +133,22 @@ get_header(); ?>
                 		</div>
             		</div>
 				
-					<?php endwhile; ?>
+					<?php endwhile; ?> -->
 					<!-- end of the loop -->
 		
 					<!-- pagination here --> 
 		
-					<?php wp_reset_postdata(); ?>
+<!-- 					<?php wp_reset_postdata(); ?>
 		
 				<?php else : ?>
 					<p><?php esc_html__( 'Sorry, no Projects have been added yet!', 'luna' ); ?></p>
 				<?php endif; ?>
         
     		</div>
-		</section>
+		</section> -->
             
             
-        <?php if ( get_theme_mod( 'active_default_home_widget' ) == '' ) : ?> 
+<!--         <?php if ( get_theme_mod( 'active_default_home_widget' ) == '' ) : ?> 
         	<?php if ( is_active_sidebar('default-home-widget-area') ) : ?>
         
         		<div class="home-widget home-widget-default shortcodes">
@@ -122,9 +160,9 @@ get_header(); ?>
                         </div><!-- .col -->
                     </div><!-- .grid -->
                 </div><!-- .home-widget -->
-                
+  <!--               
             <?php endif; ?>
-        <?php endif; ?>
+        <?php endif; ?> --> -->
 
 
 <?php get_footer(); ?>
