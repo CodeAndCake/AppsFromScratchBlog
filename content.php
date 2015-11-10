faa46d323426672350724b490a8c1c6e28e301d7
Aimee <aimeebethmj@gmail.com>
2015-11-10 16:46:16 +0000

    Content - moved image above post title and text

--- content.php
+++ content.php
@@ -12,7 +12,7 @@
                     <span>
                     
                     <!-- <h6><?php the_category(', ') ?></h6> -->
-                    
+                    <div class="archive-news-bg" style="background-image: url('<?php echo wp_get_attachment_url( get_post_thumbnail_id( $post->ID) , 'max-control' ); ?>');"></div> 
                     <h4>
                     	<a href="<?php the_permalink(); ?>">
 							<?php the_title(); ?>
@@ -36,11 +36,12 @@
             </header><!-- .entry-header --> 
     	</div>
         
+      <!--   
         <?php if (has_post_thumbnail( $post->ID ) ): ?> 
         
-    	<div class="col-1-2"> 
+ -->    	<!-- <div class="col-1-2"> 
         	<div class="archive-news-bg" style="background-image: url('<?php echo wp_get_attachment_url( get_post_thumbnail_id( $post->ID) , 'max-control' ); ?>');"></div> 
-      	</div>
+      	</div> -->
         
         <?php endif; ?>
         
