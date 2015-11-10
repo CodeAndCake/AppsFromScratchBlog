d3b5f60d1ad3df9fe15d37d8dbd8c14c260f5ea7
Aimee <aimeebethmj@gmail.com>
2015-11-10 16:07:18 +0000

    Home page - added query to pull in posts

--- template-home.php
+++ template-home.php
@@ -55,11 +55,21 @@ get_header(); ?>
 <section id="page-content-container" class="blog-post-archive">
         
 
+
     
     <div id="primary" class="content-area">
         <main id="main" class="site-main" role="main">
 
-        <?php if ( have_posts() ) : ?>
+        <?php 
+
+        /* get all the posts which category id is 1 */
+        query_posts('cat=8'); 
+
+        ?>
+
+        <?php
+
+        if ( have_posts() ) : ?>
 
             <?php /* Start the Loop */ ?>
             <?php while ( have_posts() ) : the_post(); ?>
