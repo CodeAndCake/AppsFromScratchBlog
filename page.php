48ff30e4fe66b8091ff8f4fa16c797578b119623
Aimee <aimeebethmj@gmail.com>
2015-11-12 11:51:38 +0000

    -

--- page.php
+++ page.php
@@ -53,14 +53,14 @@ get_header(); ?>
             <div id="primary" class="content-area shortcodes">
                 <main id="main" class="site-main" role="main">
         
-                        <?php get_template_part( 'content', 'page' ); ?>
+<!--                         <?php get_template_part( 'content', 'page' ); ?>
         
                         <?php
                             // If comments are open or we have at least one comment, load up the comment template
                             if ( comments_open() || get_comments_number() ) :
                                 comments_template();
                             endif;
-                        ?>
+                        ?> -->
         
                     <?php endwhile; // end of the loop. ?>
         
