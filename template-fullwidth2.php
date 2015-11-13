fb31043ac7db056f8e2760d1fa693021f3300126
Aimee <aimeebethmj@gmail.com>
2015-11-13 09:34:30 +0000

    Another test

--- template-fullwidth2.php
+++ template-fullwidth2.php
@@ -28,21 +28,53 @@ get_header(); ?>
         </div>
         <div class="entry-overlay"></div>
 	</header><!-- .entry-header -->
+
+
  
 <div class="entry-content-wrapper"> 
 	<div class="grid grid-pad page-full-contain">
     	<div class="col-1-1">    
             <div id="primary" class="content-area shortcodes">
                 <main id="main" class="site-main" role="main">
-        
-                        <?php get_template_part( 'content', 'page' ); ?>
+
+
+                    <!-- INFO PANELS -->
+                    <div class="info-panel">
+
+                    <?php if( have_rows('info_panels') ): ?>
+
+                    <?php while( have_rows('info_panels') ): the_row(); ?>
+
+
+                        <div class="grid grid-pad">
+                            <div class="col-1-2">
+                               <div class="content">
+                                   <p><?php echo ?></p>
+                               </div>
+                            </div>
+                            <div class="col-1-2">
+                               <div class="content">
+                                   <div class="archive-news-bg" style="background-image: url('<?php echo ?>');"></div>
+                               </div>
+                            </div>
+                        </div>
+
+                    <?php endwhile; ?>
+
+                    <?php endif; ?>
+
+                </div>
+
+
+        <!-- 
+                        //<?php get_template_part( 'content', 'page' ); ?>
         
                         <?php
                             // If comments are open or we have at least one comment, load up the comment template
                             if ( comments_open() || get_comments_number() ) :
                                 comments_template();
                             endif;
-                        ?>
+                        ?>// -->
         
                     <?php endwhile; // end of the loop. ?>
         
